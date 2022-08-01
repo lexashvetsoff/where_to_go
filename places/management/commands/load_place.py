24f8d45ee0c085f7bff89718d16ddb635064add6
lexashvetsoff <75941967+lexashvetsoff@users.noreply.github.com>
2022-08-01 13:03:36 +0800

    Добавлена возможность скачивания json

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -14,8 +14,10 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         file_path = kwargs['file_path']
 
-        with open(file_path, 'r', encoding='utf-8') as file:
-            place = json.load(file)
+        if 'http' == file_path[:4]:
+            response = requests.get(file_path)
+            response.raise_for_status()
+            place = json.loads(response.json())
 
             obj, created = Place.objects.get_or_create(
                 title = place['title'],
@@ -37,4 +39,29 @@ class Command(BaseCommand):
                     new_image = PlaceImage(place=new_place)
                     new_image.save()
                     new_image.image.save(image_name, ContentFile(response.content), save=True)
+
+        else:
+            with open(file_path, 'r', encoding='utf-8') as file:
+                place = json.load(file)
+
+                obj, created = Place.objects.get_or_create(
+                    title = place['title'],
+                    description_short = place['description_short'],
+                    description_long = place['description_long'],
+                    lng = place['coordinates']['lng'],
+                    lat = place['coordinates']['lat']
+                )
+                
+                if created:
+                    for img in place['imgs']:
+                        response = requests.get(img)
+                        response.raise_for_status()
+
+                        parts = img.split('/')
+                        image_name = parts[-1].split('.')[0]
+
+                        new_place = Place.objects.get(id=obj.id)
+                        new_image = PlaceImage(place=new_place)
+                        new_image.save()
+                        new_image.image.save(image_name, ContentFile(response.content), save=True)
         
