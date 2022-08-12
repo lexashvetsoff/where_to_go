2b906b62a4d71dbd9aabddbf6e287359e85d7598
lexashvetsoff <75941967+lexashvetsoff@users.noreply.github.com>
2022-08-12 15:45:56 +0800

    Высушен код

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,11 +1,9 @@
-from django.http import HttpResponse, JsonResponse, HttpRequest
-from django.shortcuts import get_object_or_404, redirect, render
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404
 from django.template import loader
 from django.urls import reverse
 
-from places.models import Place, PlaceImage
-
-import json
+from places.models import Place
 
 
 def get_serialize_places(places):
@@ -20,54 +18,13 @@ def get_serialize_places(places):
     return serialize_places
 
 
-def get_places_geojson(places, images):
-    places_geo = {}
-    for place in places:
-        filtered_images = images.filter(place=place.id).values()
-        imgs = []
-        for image in filtered_images:
-            img = 'media/{}'.format(image['image'])
-            imgs.append(img)
-        serialize_place = {
-            'title': place.title,
-            'imgs': imgs,
-            'description_short': place.description_short,
-            'description_long': place.description_long,
-            'coordinates': {
-                'lng': place.lng,
-                'lat': place.lat
-            }
-        }
-        print(imgs)
-        places_geo[place.id] = serialize_place
-    return places_geo
-
-
-def write_json(name_file, data):
-  dir_name = 'static'
-  file_path = f'{dir_name}/{name_file}.json'
-
-  with open(file_path, 'w', encoding="utf-8") as file:
-    json.dump(data, file)
-  
-  return file_path
-
-
 def index(request):
     places = Place.objects.all()
-    images = PlaceImage.objects.all()
 
-    geo_json = get_places_geojson(places, images)
     serialize_places = get_serialize_places(places)
 
     features = []
     for place in serialize_places:
-        name = place['title']
-        name_split = name.split()
-        place_file_name = '_'.join(name_split)
-        details_url = write_json(place_file_name, geo_json[place['id']])
-        # print(redirect(reverse('places', args=[place['id']])))
-        # print(redirect(reverse('places', kwargs={'place_id': place['id']})))
         serialize_features = {
             'type': "Feature",
             "geometry": {
@@ -77,8 +34,7 @@ def index(request):
             "properties": {
               "title": place['title'],
               "placeId": place['id'],
-              "detailsUrl": details_url
-            #   "detailsUrl": redirect(reverse('places', kwargs={'place_id': place['id']}))
+              "detailsUrl": reverse('places', kwargs={'place_id': place['id']})
             }
         }
         features.append(serialize_features)
@@ -103,7 +59,8 @@ def get_place_details(request, place_id):
     images = place.places.filter(place=place_id).values()
     imgs = []
     for image in images:
-        imgs.append(image['image'])
+        img = 'media/{}'.format(image['image'])
+        imgs.append(img)
 
     place_details = {
         'title': place.title,
